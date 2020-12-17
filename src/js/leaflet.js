2aa6a475b51457ff5c3dd5a1b4544a9659ef1484
Marie Bovtramovich <m.abovt@mail.ru>
2020-12-17 18:02:27 +0300

    add: leaflet.js
    add script leaflet js (init leaflet lib)
    add css for leaflet in html
    cleared scss file as not used

---
+++ src/js/leaflet.js
@@ -0,0 +1,6 @@
+const script = document.createElement('script');
+script.src = 'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js';
+script.integrity = 'sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==';
+script.crossorigin = '';
+
+document.head.append(script);
