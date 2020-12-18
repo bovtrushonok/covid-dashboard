2a5705868c658d93356c62fdbc58aa63100712e0
Marie Bovtramovich <m.abovt@mail.ru>
2020-12-18 13:57:59 +0300

    add: dark mode on and initial view

--- src/js/map_init.js
+++ src/js/map_init.js
@@ -1,11 +1,11 @@
 export default function createMap() {
   const { L } = window;
-  const Map = L.map('mapBlock').setView([51.505, -0.09], 2.3);
+  const Map = L.map('mapBlock').setView([25, 31], 2.3);
 
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoidGVkZHlrYXR6IiwiYSI6ImNraXQzeTZseDEzcXkzMWwzc3R4b2JtcGsifQ.6c4tCeNZLPWxAMUugvrPJA',
