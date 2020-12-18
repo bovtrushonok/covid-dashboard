1b322081c7f037162d0b0cbab1b6b27f2d27ba0f
kostik4kg <kostik4kg@gmail.com>
2020-12-18 17:34:34 +0300

    feat: add left sidebar

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import '../css/style.css';
 import '../css/style.scss';
-import App from './main';
+// import App from './main';
+import './left-sidebar';
 
-window.addEventListener('load', App.init());
+// window.addEventListener('load', App.init());
