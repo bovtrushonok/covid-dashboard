303f35d2a640d1424d55d665e5080d2f6ecbb9ca
Marie Bovtramovich <m.abovt@mail.ru>
2020-12-21 13:24:25 +0300

    feat: show points on map
    get ready geojSON
    add layer with points

--- src/js/map_init.js
+++ src/js/map_init.js
@@ -1,7 +1,28 @@
-export default function createMap() {
-  const { L } = window;
-  const Map = L.map('mapBlock').setView([25, 31], 2.3);
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+};
+const { L } = window;
+const Map = L.map('mapBlock').setView([25, 31], 2.3);
+let geoJson;
+const geojsonMarkerOptions = {
+  radius: 5,
+  fillColor: '#ff7800',
+  color: '#000',
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.5,
+};
 
+async function createGeoJSON() {
+  const response = await fetch('https://disease.sh/v3/covid-19/jhucsse', requestOptions);
+  const data = await response.text();
+  const result = await JSON.parse(data);
+
+  return result;
+}
+
+export default function createMap() {
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -11,5 +32,34 @@ export default function createMap() {
     accessToken: 'pk.eyJ1IjoidGVkZHlrYXR6IiwiYSI6ImNraXQzeTZseDEzcXkzMWwzc3R4b2JtcGsifQ.6c4tCeNZLPWxAMUugvrPJA',
   }).addTo(Map);
 
+  createGeoJSON()
+    .then((res) => {
+      geoJson = {
+        type: 'FeatureCollection',
+        features: res.map((countryInfo) => {
+          const { confirmed, deaths, recovered } = countryInfo.stats;
+          const { latitude, longitude } = countryInfo.coordinates;
+          return {
+            type: 'Feature',
+            properties: {
+              ...countryInfo,
+            },
+            geometry: {
+              type: 'Point',
+              coordinates: [longitude, latitude],
+              stats: [confirmed, deaths, recovered],
+            },
+          };
+        }),
+      };
+      return geoJson;
+    })
+    .then((geojson) => {
+      L.geoJSON(geojson, {
+        pointToLayer(feature, latlng) {
+          return L.circleMarker(latlng, geojsonMarkerOptions);
+        },
+      }).addTo(Map);
+    });
   return Map;
 }
