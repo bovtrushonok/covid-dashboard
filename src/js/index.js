6826cc3f86e2afe468e18c83afaba23489548777
tonykosmos <a.kosmovskiy@gmail.com>
2020-12-21 17:33:46 +0300

    fix: remove eslint errors

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
 import '../css/style.css';
 import '../css/style.scss';
 // import App from './main';
+import './statistic-table';
+import './left-sidebar';
+// window.addEventListener('load', App.init());
